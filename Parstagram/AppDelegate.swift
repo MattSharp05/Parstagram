f0a918b81129739c209f8f7da2009b7d73ab135f
MattSharp05 <45979221+MattSharp05@users.noreply.github.com>
2022-01-07 21:19:19 -0500

    Parstagram

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "A4M1xH2nfFbXrab4VRo0VizVAQYckukxNIUCt6HG"
+                $0.clientKey = "yLT5eizLIdJNAjQL51xzo6KZCWpKa2L6Wtp046Os" 
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+                
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Parstagram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Parstagram
+  # This pulls in the latest version of Alamofire. 
+  pod 'AlamofireImage'
+  # This pulls in the latest version of Parse. 
+  pod 'Parse'
+
+end
